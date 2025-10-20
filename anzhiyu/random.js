8c091b6065e51b71b4953f32d397207841cf0401
syzh0675 <181959331@qq.com>
2025-10-20 16:07:39 +0800

    Site updated: 2025-10-20 16:07:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/20/hexo安装1/","2025/10/20/hello-world/"];function toRandomPost(){
+var posts=["2025/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
