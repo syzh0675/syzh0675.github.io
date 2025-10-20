e47fecc29d092651de86fa5f4af7858132242ea7
syzh0675 <181959331@qq.com>
2025-10-20 19:19:25 +0800

    Site updated: 2025-10-20 19:19:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/20/wodediyipobiaofe/","2025/10/20/hello-world/"];function toRandomPost(){
+var posts=["2025/10/20/我的个人简介/","2025/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
