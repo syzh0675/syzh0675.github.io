0b5f30325c1436b9040b5efec876836ca92ca40d
syzh0675 <181959331@qq.com>
2025-10-20 14:11:54 +0800

    Site updated: 2025-10-20 14:11:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/20/hello-world/"];function toRandomPost(){
+var posts=["2025/10/20/hexo安知鱼主题设置/","2025/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
