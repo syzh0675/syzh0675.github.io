2ccb07ea468ddda11114c0d5db6c82a6a11262e4
syzh0675 <181959331@qq.com>
2025-10-21 15:42:19 +0800

    Site updated: 2025-10-21 15:42:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/20/安知鱼主题音乐馆默认歌单修改/","2025/10/20/hello-world/"];function toRandomPost(){
+var posts=["2025/10/21/群晖搭建电影网站/","2025/10/20/安知鱼主题音乐馆默认歌单修改/","2025/10/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
