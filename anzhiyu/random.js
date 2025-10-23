c6d29e1144f0b52303d12306191cd581e433b866
syzh0675 <181959331@qq.com>
2025-10-23 15:13:39 +0800

    Site updated: YYYY-10-2025年10月23日 15:13:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/21/一声佛号一声心/","2025/10/21/群晖搭建电影网站/","2025/10/20/安知鱼主题音乐馆默认歌单修改/"];function toRandomPost(){
+var posts=["2025/10/23/群晖DSM利用webstation部署h5ai-1761201936096/","2025/10/21/一声佛号一声心/","2025/10/21/群晖搭建电影网站/","2025/10/20/安知鱼主题音乐馆默认歌单修改/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
