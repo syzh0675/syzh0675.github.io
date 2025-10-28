7b81ec1045c7b1146caeec19c90ff7b43ffda3ac
syzh0675 <181959331@qq.com>
2025-10-28 17:55:48 +0800

    Site updated: YYYY-10-2025年10月28日 17:55:47

--- js/utils.js
+++ js/utils.js
@@ -845,7 +845,7 @@ const anzhiyu = {
       return;
     }
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "2128374668";
+    const userId = "8460446202";
     const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
